b32bdf2f2369b3c1cf402065d7f803c9e09a940a
Rafi Septian Hadi <rafiseptian90@gmail.com>
2024-01-20 17:52:14 +0700

    feat: Add docker configuration

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Client } = require('pg');
 
 const client = new Client({
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: "demonx",
-    database: "MyFinance",
+    host: `${process.env.DB_HOST}`,
+    port: `${process.env.DB_PORT}`,
+    user: `${process.env.DB_USER}`,
+    password: `${process.env.DB_PASSWORD}`,
+    database: `${process.env.DB_NAME}`,
 });
 
 client.connect();
